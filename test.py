a28c91ae525cefed2a7d9ef38cd4cfbbbed783ad
Taku75 <37369400+Taku75@users.noreply.github.com>
2018-08-18 00:12:56 +0900

    test

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 import pigpio
+from omni_whiles import Omni_Control
 from motor import Motor
 
-pi=pigpio.pi()
-motor=Motor(pi, 12, 13)
+pi = pigpio.pi()
+ur_motor = Motor(pi,)
+ul_motor = Motor(pi,)
+dr_motor = Motor(pi,)
+dl_motor = Motor(pi,)
+
+motors = [ur_motor, ul_motor, dl_motor, dr_motor]
+
+while True:
+    Omni_Control(motors, )
+    
