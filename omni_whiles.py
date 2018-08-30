f79c102565a7dbda0398d75d5e307c7a52a3636b
Taku75 <ta9mizoids@gmail.com>
2018-08-31 05:02:23 +0900

    omni wheel control test

--- omni_whiles.py
+++ omni_whiles.py
@@ -1,39 +1,41 @@
 import pigpio
 from motor import Motor
-from typing import List
 from math import *
 import numpy as np
 
-omuni=List[Motor]
-
 class Omni_Control:
-
-    ALPHA_ = pi()/4
-
-    def __init__(self, motors: omuni, radius):
-        self._motors = {"Right-Front":moters[0], "Right-Back":motors[1], "Left-Front":motors[2], "Left-Back":motors[3]}
-        self._motors_key = list(_motors.keys())
-        self._motor_list = list(_motors.values())
-        self._speed = {"Right-Front":motors[0].speed, "Right-Back":motors[1].speed, "Left-Front":motors[2].speed, "Left-Back":motors[3].speed}
+    def __init__(self, motors, radius):
+        print(motors)
+        self._motors = motors
+        #self._motors_key = list(self._motors.keys())
+        #self._motor_list = list(self._motors.values())
+        self._speed = [motors[0].speed, motors[1].speed, motors[2].speed, motors[3].speed]
         self.__radius = radius
         self._range = motors[0].range
-
+        self._vx = 0
+        self._vy = 0
+        self._omega = 0
 
     @property
-    def speed(self, key = "All": str):
+    def speed(self, key = "All"):
         if key == "All":
             return self._speed
-        return self._speed.get(key)
 
-    @speed.setter
-    def speed(self, vx, vy, omega):
-        A = np.array([[-sin(self.ALPHA_), sin(self.ALPHA_), self.__radius],
-                    [-cos(self.ALPHA_), -cos(self.ALPHA_), self.__radius],
-                    [sin(self.ALPHA_), -sin(self.ALPHA_), self.__radius],
-                    [cos(self.ALPHA_), cos(self.ALPHA_), self.__radius]])
-        motor_array=np.array(self._motor_list)
-        speed = np.dot(A, motor_array)
-        self._motors[self._motors_key[0]].speed = speed[0]*self._range
-        self._motors[self._motors_key[1]].speed = speed[1]*self._range
-        self._motors[self._motors_key[2]].speed = speed[2]*self._range
-        self._motors[self._motors_key[3]].speed = speed[3]*self._range
+
+    def setspeed(self, vx, vy, omega):
+        ALPHA_ = pi/4
+        A = np.array([[-sin(ALPHA_), sin(ALPHA_), self.__radius],
+                    [-cos(ALPHA_), -cos(ALPHA_), self.__radius],
+                    [sin(ALPHA_), -sin(ALPHA_), self.__radius],
+                    [cos(ALPHA_), cos(ALPHA_), self.__radius]])
+        motor_array=np.array([vx,vy,omega])
+        _speed = np.dot(A, motor_array)
+        _speed = _speed.tolist()
+        self._speed[0] = _speed[0]
+        self._speed[1] = _speed[1]
+        self._speed[2] = _speed[2]
+        self._speed[3] = _speed[3]
+        self._motors[0].speed = self._speed[0]*self._range
+        self._motors[1].speed = self._speed[1]*self._range
+        self._motors[2].speed = self._speed[2]*self._range
+        self._motors[3].speed = self._speed[3]*self._range
--- test.py
+++ test.py
@@ -1,15 +1,70 @@
 import pigpio
 from omni_whiles import Omni_Control
 from motor import Motor
+import msvcrt
+import time
+from decimal import Decimal
 
-pi = pigpio.pi()
-ur_motor = Motor(pi,)
-ul_motor = Motor(pi,)
-dr_motor = Motor(pi,)
-dl_motor = Motor(pi,)
+"""pi = pigpio.pi()
+ur_motor = Motor(pi,14,15)
+ul_motor = Motor(pi,17,18)
+dr_motor = Motor(pi,22,23)
+dl_motor = Motor(pi,9,25)
 
 motors = [ur_motor, ul_motor, dl_motor, dr_motor]
-
+omni_control = Omni_Control(motors, 0.5)"""
+vx = 0
+vy = 0
+omega = 0
 while True:
-    Omni_Control(motors, )
-    
+    time.sleep(0.5)
+    if msvcrt.kbhit():
+        key = msvcrt.getch()
+        print(key)
+        if key == b'\00':
+            continue
+        key = key.decode()
+        print("pushed {0}".format(key))
+
+        if key == 'd':
+            vx += 0.10
+            if vx > 1.0:
+                vx = 1.0
+        elif key == 'a':
+            vx -= 0.10
+            if vx < -1.0:
+                vx = -1.0
+
+        if key == 'w':
+            vy += 0.10
+            if vy > 1.0:
+                vy = 1.0
+        elif key == 's':
+            vy -= 0.10
+            if vy < -1.0:
+                vy = -1.0
+
+        if key == 'q':
+            omega = 1.0
+        elif key == 'e':
+            omega = -1.0
+    else:
+        if vx > 0.01:
+            vx -= 0.10
+        elif vx < -0.01:
+            vx += 0.10
+        else:
+            vx = 0
+
+        if vy > 0.01:
+            vy -= 0.10
+        elif vy < -0.01:
+            vy += 0.10
+        else:
+            vy = 0
+
+        omega = 0.0
+
+    print(str(vx) + " " + str(vy) + " " + str(omega))
+
+        #omni_control.setspeed(vx, vy, omega)
